eab8d8dd5d702d6e67899e6695dac5de6565c2a4
Laurianne <valette.laurianne@gmail.com>
2022-05-06 15:51:54 +0200

    signup, login, ajout, modification et suppression de sauces

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://admin:81otKOYKfei1FV4D@cluster0.eb8oj.mongodb.net/database?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
